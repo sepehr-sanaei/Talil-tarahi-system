963c7c430e88f005f97fa9684966dae3d0a32a1f
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2024-05-25 22:39:07 +0330

    added wallet and rofile models

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.user.username
+
+class Wallet(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name='wallet')
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"{self.profile.user.username}'s Wallet"
+
+# Signal to create Wallet when Profile is created
+@receiver(post_save, sender=Profile)
+def create_or_update_wallet(sender, instance, created, **kwargs):
+    if created:
+        Wallet.objects.create(profile=instance)
+    instance.wallet.save()
